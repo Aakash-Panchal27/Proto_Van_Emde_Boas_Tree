2abc4f814776c2259050307898fe9b2a2390b2a6
27Akmd <44540708+27Akmd@users.noreply.github.com>
2019-07-22 17:37:16 +0530

    comments added

--- Van Emde Boas Tree.cpp
+++ Van Emde Boas Tree.cpp
@@ -57,35 +57,55 @@ public:
 
 bool isMember(Van_Emde_Boas* helper, int key)
 {
-
+  //if key is greter then universe_size then
+  //returns false
   if(key >= helper->universe_size)
     return false;
 
+  //if we reach at base case
+  //the just return wheather
+  //pointer is nullptr then false
+  //else return true
   if(helper->universe_size == 2)
   {
     return helper->clusters[key];
   }
   else
-  return isMember( helper->clusters[ helper->high(key) ], helper->low(key) );
+  {
+    //recursively goes deep in to the
+    //level of VEB tree using its
+    //cluster index and its position
+    return isMember( helper->clusters[ helper->high(key) ], helper->low(key) );
+  }
 }
 
 void insert(Van_Emde_Boas* &helper, int key)
 {
+  //If we reach at base case
+  //then assign VEB(1) in place
+  //of nullptr
   if(helper->universe_size == 2)
   {
     helper->clusters[key] = new Van_Emde_Boas(1);
   }
   else
   {
+      //recursively using index of cluster and its
+      //position in cluster
       insert( helper->clusters[ helper->high(key) ], helper->low(key) );
+
+      //also do same recusion in summary VEB
       insert(helper->summary, helper->high(key) );
   }
 }
 
+
+
 int main()
 {
   Van_Emde_Boas*hello = new Van_Emde_Boas(4);
-  cout << isMember(hello, 4);
+
+  cout << isMember(hello, 3);
 
   insert(hello, 3);
 
